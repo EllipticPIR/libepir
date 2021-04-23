2151471b831261b38fa463db3774358d47104985
Vis Virial <visvirial@gmail.com>
2021-04-23 18:32:56 +0900

    Add "await".

--- src/test_napi.ts
+++ src/test_napi.ts
@@ -16,21 +16,21 @@ const time = () => new Date().getTime();
 	// selector_create().
 	const index_counts = [1000, 1000, 1000];
 	const beginSelectorsCreate = time();
-	const selector = epir.selector_create(pubkey, index_counts, 1024);
+	const selector = await epir.selector_create(pubkey, index_counts, 1024);
 	console.log(`Selector created (normal) in ${(time() - beginSelectorsCreate).toLocaleString()}ms.`);
 	// selector_create_fast().
 	const beginSelectorsFastCreate = time();
-	const selectorFast = epir.selector_create_fast(privkey, index_counts, 1024);
+	const selectorFast = await epir.selector_create_fast(privkey, index_counts, 1024);
 	console.log(`Selector created (fast) in ${(time() - beginSelectorsFastCreate).toLocaleString()}ms.`);
 	// reply_decrypt().
-	const beginDecrypt = time();
 	const data = require('../src/test_napi_reply_data.json');
+	const beginDecrypt = time();
 	const decrypted = await epir.reply_decrypt(new Uint8Array(data.reply), new Uint8Array(data.privkey), data.dimension, data.packing);
+	console.log(`Reply decrypted in ${(time() - beginDecrypt).toLocaleString()}ms.`);
 	for(let i=0; i<data.correct.length; i++) {
 		if(decrypted[i] != data.correct[i]) {
 			throw new Error('Decrypted is not correct.');
 		}
 	}
-	console.log(`Reply decrypted in ${(time() - beginDecrypt).toLocaleString()}ms.`);
 })();
 
