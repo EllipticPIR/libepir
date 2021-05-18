9dd8af340c4bb727c316224c4e76d9c9de0129d7
Vis Virial <visvirial@gmail.com>
2021-05-18 22:57:01 +0900

    Fix WebAssembly definition.

--- src_ts/wasm.libepir.ts
+++ src_ts/wasm.libepir.ts
@@ -3,9 +3,9 @@ export type LibEpir = {
 	HEAPU8: Uint8Array,
 	_malloc: (len: number) => number,
 	_free: (buf: number) => void,
-	addFunction: (func: (...args: any[]) => any) => number,
+	addFunction: (func: (...args: any[]) => any, signature: string) => number,
 	removeFunction: (buf: number) => void,
-}
+} & { [func: string]: (...args: any[]) => any; };
 export type LibEpirModule = (() => Promise<LibEpir>);
 
 export const libEpirModule = require('../dist/libepir') as LibEpirModule;
