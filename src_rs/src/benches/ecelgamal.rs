ddacf01c3710c3fc4fbe40ef1222658a521400bc
Vis Virial <visvirial@gmail.com>
2021-06-10 20:08:26 +0900

    Add `bench_generate()`.

--- src_rs/src/benches/ecelgamal.rs
+++ src_rs/src/benches/ecelgamal.rs
@@ -2,6 +2,17 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 use epir::ecelgamal::*;
 
+fn bench_load_mg(c: &mut Criterion) {
+    let mut group = c.benchmark_group("load_mg");
+    group.sample_size(10);
+    group.bench_function("load_mg", |b| {
+        b.iter(|| {
+            DecryptionContext::load_from_file(None).unwrap();
+        })
+    });
+    group.finish();
+}
+
 fn bench_encrypt(c: &mut Criterion) {
     let privkey = PrivateKey::new();
     let pubkey = PublicKey::new(&privkey);
@@ -17,17 +28,6 @@ fn bench_encrypt(c: &mut Criterion) {
     });
 }
 
-fn bench_load_mg(c: &mut Criterion) {
-    let mut group = c.benchmark_group("load_mg");
-    group.sample_size(10);
-    group.bench_function("load_mg", |b| {
-        b.iter(|| {
-            DecryptionContext::load_from_file(None).unwrap();
-        })
-    });
-    group.finish();
-}
-
 fn bench_decrypt(c: &mut Criterion) {
     let privkey = PrivateKey::new();
     let cipher = privkey.encrypt(&1234u32.into(), None);
@@ -39,5 +39,26 @@ fn bench_decrypt(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_encrypt, bench_decrypt, bench_load_mg);
+const MMAX_MOD: u8 = 16;
+const MMAX: u32 = 1 << MMAX_MOD;
+
+fn bench_generate(c: &mut Criterion) {
+    let mut group = c.benchmark_group("generate");
+    let mut mgs = Vec::new();
+    group.sample_size(10);
+    group.bench_function("no_sort", |b| {
+        b.iter(|| {
+            mgs = DecryptionContext::generate_no_sort(Some(MMAX), |_| {});
+        })
+    });
+    group.sample_size(100);
+    group.bench_function("sort", |b| {
+        b.iter(|| {
+            DecryptionContext::generate_sort(&mut mgs);
+        })
+    });
+    group.finish();
+}
+
+criterion_group!(benches, bench_load_mg, bench_encrypt, bench_decrypt, bench_generate);
 criterion_main!(benches);
