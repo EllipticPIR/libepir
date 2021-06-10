0d9ef55f8b25c184c856f8373c60a0ccd8fc4524
Vis Virial <visvirial@gmail.com>
2021-06-10 18:06:33 +0900

    Add `load_mg` benchmark.

--- src_rs/src/benches/ecelgamal.rs
+++ src_rs/src/benches/ecelgamal.rs
@@ -18,18 +18,28 @@ fn bench_encrypt(c: &mut Criterion) {
     });
 }
 
+fn bench_load_mg(c: &mut Criterion) {
+    let mut group = c.benchmark_group("load_mg");
+    group.sample_size(10);
+    group.bench_function("load_mg", |b| {
+        b.iter(|| {
+            DecryptionContext::load_from_file(None).unwrap();
+        })
+    });
+    group.finish();
+}
+
 fn bench_decrypt(c: &mut Criterion) {
     let privkey = PrivateKey::new();
-    let pubkey = PublicKey::new(&privkey);
     let enc_ctx = EncryptionContext::new();
     let cipher = privkey.encrypt(&enc_ctx, &1234u32.into(), None);
     let dec_ctx = DecryptionContext::load_from_file(None).unwrap();
     c.bench_function("decrypt", |b| {
         b.iter(|| {
-            dec_ctx.decrypt_cipher(&privkey, &cipher);
+            dec_ctx.decrypt_cipher(&privkey, &cipher).unwrap();
         })
     });
 }
 
-criterion_group!(benches, bench_encrypt, bench_decrypt);
+criterion_group!(benches, bench_encrypt, bench_decrypt, bench_load_mg);
 criterion_main!(benches);
