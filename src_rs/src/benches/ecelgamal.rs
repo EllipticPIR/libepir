97b9651563cd8acd24f9f7d0ebdf0c689ecedc65
Vis Virial <visvirial@gmail.com>
2021-06-10 15:35:06 +0900

    Add benchmark for decryption.

--- src_rs/src/benches/ecelgamal.rs
+++ src_rs/src/benches/ecelgamal.rs
@@ -3,14 +3,33 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use epir::ecelgamal::*;
 
 fn bench_encrypt(c: &mut Criterion) {
+    let privkey = PrivateKey::new();
+    let pubkey = PublicKey::new(&privkey);
+    let enc_ctx = EncryptionContext::new();
+    c.bench_function("encrypt_normal", |b| {
+        b.iter(|| {
+            pubkey.encrypt(&enc_ctx, &1234u32.into(), None);
+        })
+    });
     c.bench_function("encrypt_fast", |b| {
-        let privkey = PrivateKey::new();
-        let enc_ctx = EncryptionContext::new();
         b.iter(|| {
             privkey.encrypt(&enc_ctx, &1234u32.into(), None);
         })
     });
 }
 
-criterion_group!(benches, bench_encrypt);
+fn bench_decrypt(c: &mut Criterion) {
+    let privkey = PrivateKey::new();
+    let pubkey = PublicKey::new(&privkey);
+    let enc_ctx = EncryptionContext::new();
+    let cipher = privkey.encrypt(&enc_ctx, &1234u32.into(), None);
+    let dec_ctx = DecryptionContext::load_from_file(None).unwrap();
+    c.bench_function("decrypt", |b| {
+        b.iter(|| {
+            dec_ctx.decrypt_cipher(&privkey, &cipher);
+        })
+    });
+}
+
+criterion_group!(benches, bench_encrypt, bench_decrypt);
 criterion_main!(benches);
